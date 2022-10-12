86e6e711f7c71ec15ce1dd13c1e9aa7d51547003
Viktor Lavrenenko <lavrenenkoviktoren@gmail.com>
2022-10-12 15:25:40 +0300

    Add hardhat-gas-reporter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,17 @@
+import * as dotenv from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
+import "hardhat-gas-reporter"
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.13",
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
 };
 
 export default config;
